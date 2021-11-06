86d0d637be608f3e6acd37f2edcf89f9d9fff7e6
josesilva04 <miccolijr@gmail.com>
2021-11-06 21:04:11 +0000

    First_Commit

---
+++ derivatives.py
@@ -0,0 +1,30 @@
+from manim import *
+
+# A insistir --> a derivada é uma taxa de variação infinitesimal de uma dada função f
+# COMPREENDER O INFINITÉSIMO!!!
+
+
+#PART01
+#TODO: Texto da aula --> Apresentação do conceito formal
+#TODO: Função polinomial em 0xy --> Taxa média de variação --> Noção intuitiva --> Noção formal
+#TODO: Transform num gráfico 0tx --> Ponto a mover-se num referencial unidimensional
+#TODO: Variação num instante --> paradoxo, não há duas medidas num instante --> então temos o "limite", quando a variação se torna mais pequena
+#TODO: Variação infinitesimal --> Reta tangente ao gráfico da função --> Variação infinitesimal
+#TODO: Acompanhar a derivada em cada ponto com o gráfico da derivada --> Replay da animação --> análise ruber-duck
+#TODO: Apresentar a fórmula --> *FORMULA TEXT HERE* --> voltar a associar com a variação infinitesimal e tangente
+#TODO: RECAP DOS CONCEITOS GERAIS
+#TODO: FINISH UP PART 01
+#PART02
+#TODO: RECAP da aula anterior 
+#TODO: Análise da monotonia recorrendo à derivada --> Reforçar a visualização dy/dx  --> Tabela --> Associar a tabela com o gráfico (Dot Mobject) --> replay
+#TODO: Problema de otimização --> resolução acompanhada --> deduzir fórmulas em openboard --> passar à visualização
+#TODO: RECAP DOS CONCEITOS GERAIS
+#PART03: OPTIONAL
+#TODO: DERIVAÇÃO DA REGRA DO POLINÓMIO
+#TODO: FURTER RESEARCH --> VÍDEOS 3BLUE1BROWN SOBRE A CHAIN RULE E DERIVADAS
+
+def Derivatives(Scene):
+    def constructor(self):
+
+        # Text mobject
+        t = Tex()
